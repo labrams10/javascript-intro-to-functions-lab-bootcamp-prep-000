a372b1a3c9b404d30caf2ca6018f0d3ff373fa71
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-04-16 16:43:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function whisper(string) {return string.toLowerCase()}
+function shout(string) {return string.toUpperCase()}
+function logShout(string){
+  console.log(string.toUpperCase())
+  }
+function sayHiToGrandma(string){
+  if (string == string.toLowerCase()){
+    return "I can't hear you!"
+  }
+}
